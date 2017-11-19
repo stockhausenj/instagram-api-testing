9a52fec068ff6f0e60471d2b5a8b84bfdae64f29
Jay Stockhausen <jay.stockhausen@gmail.com>
2017-11-19 23:31:04 +0000

    Display username on pug homepage after authorization.

--- server/app.js
+++ server/app.js
@@ -1,19 +1,81 @@
-// Import modules.
-const express = require("express"),
-	path = require("path"),
-	bodyParser = require("body-parser"),
-	assert = require("assert");
+"use strict";
+
+// Make modules available.
+const express = require('express'),
+			path = require('path'),
+			fs = require("fs"),
+			instagram = require('node-instagram').default;
+
+// File System object returns contents of the path.
+const creds = fs.readFileSync(path.join(__dirname, "instagram.creds"));
+const jsonContent = JSON.parse(creds);
+const igId = jsonContent.id;
+const igSecret = jsonContent.secret;
 
 // Create express application.
-const app = express();
+const	app = express();
 
-// Enables the return of an object that contains the parsed request body.
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "pug");
 
-app.set('view engine', 'pug')
-app.set("views", path.join(__dirname, "/../client"));
+const clientPath = path.join(__dirname, '/../client');
 
-app.get("/", function(req, res) {
+// Express lookups files relative to the static directory so that it's not included in the URL.
+app.use(express.static(clientPath));
+app.set("views", clientPath);
+
+const ig = new instagram({
+	clientId: igId,
+	clientSecret: igSecret
+});
+
+// Your redirect url where you will handle the code param
+const redirectUri = 'http://18.216.205.96:8080/handleauth';
+
+app.use(function(req, res, next) {
+	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+	console.log(fullUrl, 'debug');
+	console.log(JSON.stringify(req.params), 'debug');
+	console.log(JSON.stringify(req.query), 'debug');
+	next();
+});
+
+app.get('/', function(req, res) {
+	ig.get('users/self', (err, data) => {
+		console.log(data);
+		if (err) {
+			res.redirect('/authorize_user');
+		} else {
+			res.render("index", { username: data.data.username });
+		}
+	});
+});
+
+// First redirect user to instagram oauth
+app.get('/authorize_user', (req, res) => {
+	res.redirect(ig.getAuthorizationUrl(redirectUri, {
+		// an array of scopes
+		scope: ['basic', 'likes'],
+		// an optional state
+		state: 'your state',
+	}));
+});
+
+// Handle auth code and get access_token for user
+app.get('/handleauth', async (req, res) => {
+	try {
+		// The code from the request, here req.query.code for express
+		const code = req.query.code;
+		const data = await ig.authorizeUser(code, redirectUri);
+		// data.access_token contain the user access_token
+		//res.json(data);
+		ig.config.accessToken = data.access_token;
+		res.redirect('/');
+	} catch (err) {
+		res.json(err);
+	}
+});
+
+app.get('*', (req, res) => {
 	res.render("index");
 });
 
