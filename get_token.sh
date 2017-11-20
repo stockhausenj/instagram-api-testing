9a5ba5351e34f77e9bc2868e0f09789ff6e85d5e
Jay Stockhausen <jay.stockhausen@gmail.com>
2017-11-20 03:46:10 +0000

    Ensure creds file isn't uploaded.

--- get_token.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -F 'client_id=639a0da1d3bd4962aed72ff34c19c394' \
-    -F 'client_secret=$2' \
-    -F 'grant_type=authorization_code' \
-    -F 'redirect_uri=http://18.216.205.96:8080' \
-    -F 'code=$1' \
-    https://api.instagram.com/oauth/access_token
