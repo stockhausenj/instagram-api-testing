18f97df5d5d12a5aad739dd8062a2cae9582d94c
Jay Stockhausen <jay.stockhausen@gmail.com>
2017-11-21 04:04:44 +0000

    Update linter config and fix lint errors.

--- server/components/instagram/index.js
+++ server/components/instagram/index.js
@@ -1,9 +1,9 @@
 "use strict";
 
 // Make modules available.
-const instagram = require('node-instagram').default,
-	path = require('path'),
-	fs = require('fs');
+const instagram = require("node-instagram").default,
+  path = require("path"),
+  fs = require("fs");
 
 const creds = fs.readFileSync(path.join(__dirname, "instagram.creds"));
 const jsonContent = JSON.parse(creds);
@@ -11,8 +11,8 @@ const igId = jsonContent.id;
 const igSecret = jsonContent.secret;
 
 const ig = new instagram({
-	  clientId: igId,
-	  clientSecret: igSecret
+  clientId: igId,
+  clientSecret: igSecret
 });
 
 module.exports = ig;
